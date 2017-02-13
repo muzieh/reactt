ee796356f8540fd4361b2758b4517e4da4bc9e67
Marcin Owerczuk <muzieh@Marcin-Owerczuks-MacBook.local>
2017-02-13 00:48:59 +0000

    tickin clock

--- entry.js
+++ entry.js
@@ -2,15 +2,17 @@ console.log("test");
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Welcome(props) {
-      return <div>
-                  <h1>Hello, {props.name}</h1>
-                  <div>Jajo {props.kajo}</div>
-               </div>;
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+        );
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    );
 }
 
-const element = <Welcome name="Sara" kajo="bajo"/>;
-ReactDOM.render(
-  element,
-    document.getElementById('root')
-    );
+setInterval(tick, 1000);
